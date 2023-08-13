6ca2bdfe34c88737b209b45e4ecdfbfc8f0a458c
nikish228xg <nikish228xg@gmail.com>
2023-08-13 20:08:46 +0700

    fix: исправлена структура проекта.

--- src/ContactsApp/ContactsApp.Model/Class1.cs
+++ src/ContactsApp/ContactsApp.Model/Class1.cs
@@ -1,7 +1,12 @@
-﻿namespace ContactsApp.Model
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactsApp.Model
 {
     public class Class1
     {
-
     }
 }
